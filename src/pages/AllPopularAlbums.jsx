6364e5d4440ac263b90f67871a1ad0f5c4040c0d
muhammadarii <aripurnomo606@gmail.com>
2024-08-04 13:57:46 +0700

    Update Progress

--- src/pages/AllPopularAlbums.jsx
+++ src/pages/AllPopularAlbums.jsx
@@ -6,17 +6,19 @@ import PopularAlbums from "../components/ShowAll/PopularAlbums";
 
 const AllPopularAlbums = () => {
   return (
-    <div className="flex flex-col h-screen bg-gradient-to-t from-[#121212] to-[#181818]">
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
-        <div className="flex-1 flex flex-col overflow-y-scroll scrollbar-hide">
-          <Navbar />
-          <div className="flex-1 overflow-y-auto scrollbar-hide mr-3">
-            <PopularAlbums />
+    <div className="container mx-auto">
+      <div className="flex flex-col h-screen bg-gradient-to-t from-[#121212] to-[#181818]">
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar />
+          <div className="flex-1 flex flex-col overflow-y-scroll scrollbar-hide">
+            <Navbar />
+            <div className="flex-1 overflow-y-auto scrollbar-hide mr-3">
+              <PopularAlbums />
+            </div>
           </div>
         </div>
+        <Footer className="w-full" />
       </div>
-      <Footer className="w-full" />
     </div>
   );
 };
--- src/pages/AllPopularArtist.jsx
+++ src/pages/AllPopularArtist.jsx
@@ -6,17 +6,19 @@ import PopularArtist from "../components/ShowAll/PopularArtist";
 
 const AllPopularArtist = () => {
   return (
-    <div className="flex flex-col h-screen bg-gradient-to-t from-[#121212] to-[#181818]">
-      <div className="flex flex-1 overflow-hidden">
-        <Sidebar />
-        <div className="flex-1 flex flex-col overflow-y-scroll scrollbar-hide">
-          <Navbar />
-          <div className="flex-1 overflow-y-auto scrollbar-hide mr-3">
-            <PopularArtist />
+    <div className="container mx-auto">
+      <div className="flex flex-col h-screen bg-gradient-to-t from-[#121212] to-[#181818]">
+        <div className="flex flex-1 overflow-hidden">
+          <Sidebar />
+          <div className="flex-1 flex flex-col overflow-y-scroll scrollbar-hide">
+            <Navbar />
+            <div className="flex-1 overflow-y-auto scrollbar-hide mr-3">
+              <PopularArtist />
+            </div>
           </div>
         </div>
+        <Footer className="w-full" />
       </div>
-      <Footer className="w-full" />
     </div>
   );
 };
